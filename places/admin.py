0f35bbe7dfdca25ce4b6b25204dc2f8b5d1ccdf0
Your Name <you@example.com>
2024-04-23 16:26:35 +0500

    get_preview image

--- places/admin.py
+++ places/admin.py
@@ -1,15 +1,36 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from places.models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
+    readonly_fields = ('get_preview',)
+    fields = ('id', 'image', 'get_preview',)
+
+    def get_preview(self, obj):
+        max_height = 200
+        height = obj.image.height
+        if height > max_height:
+            height = 200
+
+        return mark_safe('<img src="{url}" height={height} />'.format(
+            url=obj.image.url,
+            height=height,
+        ))
 
 
 @admin.register(Place)
-class SubdivisionAdmin(admin.ModelAdmin):
-    # list_display = ['coordinates']
+class PlaceAdmin(admin.ModelAdmin):
     inlines = [PlaceImageInline]
+    readonly_fields = ('image',)
+
+    def image(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.headshot.url,
+            width=obj.headshot.width,
+            height=obj.headshot.height,
+        ))
 
 admin.site.register(PlaceImage)
