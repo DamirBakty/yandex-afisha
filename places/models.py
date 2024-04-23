1069c9af22e12d83dcd40385d583946913f5de9d
Your Name <you@example.com>
2024-04-23 10:50:23 +0500

    add Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.gis.db.models import PointField
 
 # Create your models here
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=255)
+    description_short = models.CharField(max_length=255)
+    description_long = models.TextField()
+    coordinates = models.JSONField(default=dict())
+
+    def __str__(self):
+        return self.title
