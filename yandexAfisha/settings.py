0df96f1b41758a683df18bf9409e0d4f69977873
Your Name <you@example.com>
2024-04-24 10:26:37 +0500

    add .env

--- yandexAfisha/settings.py
+++ yandexAfisha/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%k*rpn!6av-n-cn%t^4j5=5gu6t*l#tk3ovfsk))6b1%x90=kd'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
