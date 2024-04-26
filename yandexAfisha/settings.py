51927d19bd9262613d6f921757aa849f35b941c2
Your Name <you@example.com>
2024-04-26 11:00:30 +0500

    Default values

--- yandexAfisha/settings.py
+++ yandexAfisha/settings.py
@@ -18,9 +18,9 @@ env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY', default='NOT_SECRET')
 
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', default=True)
 
 
 ALLOWED_HOSTS = ['*']
